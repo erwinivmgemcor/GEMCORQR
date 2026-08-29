4c782e4ccb63c377c9e6b6e84a805433020ec51f
erwinivmgemcor <erwin.ivmgemcor@gmail.com>
2026-08-30 07:05:12 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1621,8 +1621,8 @@ itemsHtml += '<tr>' +
 '</tr>';
 });
 
-// Fill remaining rows (minimum 10 data rows for print aesthetics)
-var totalRows = Math.max(10, items.length);
+// Fill remaining rows (30 total data rows to match Google Sheets template)
+var totalRows = Math.max(30, items.length);
 for (var i = items.length; i < totalRows; i++) {
 itemsHtml += '<tr><td class="td-center">&nbsp;</td><td class="td-center">&nbsp;</td><td class="td-center">&nbsp;</td><td class="td-left">&nbsp;</td><td class="td-center">&nbsp;</td><td class="td-center">&nbsp;</td><td class="td-center">&nbsp;</td><td class="td-center">&nbsp;</td></tr>';
 }
@@ -1630,32 +1630,36 @@ itemsHtml += '<tr><td class="td-center">&nbsp;</td><td class="td-center">&nbsp;<
 // "no further entries" row
 itemsHtml += '<tr><td class="td-center">&nbsp;</td><td class="td-center">&nbsp;</td><td class="td-center">&nbsp;</td><td class="td-left" style="font-size:7pt;text-align:center;">******************** no further entries below this line ********************</td><td class="td-center">&nbsp;</td><td class="td-center">&nbsp;</td><td class="td-center">&nbsp;</td><td class="td-center">&nbsp;</td></tr>';
 
-// Use template literal to avoid quote escaping hell
+var mrifQrUrl = 'https://api.qrserver.com/v1/create-qr-code/?size=120x120&data=' + encodeURIComponent(docNo);
+
 var html = `<div class="mrif-print-sheet">
 <div class="mrif-header">
 <div class="mrif-logo"><img src="gemcor-logo.png" alt="GEMCOR"></div>
-<div class="mrif-docno"><span class="mrif-dn-label">MRIF No.:</span><span class="mrif-dn-box">${docNo}</span></div>
+<div class="mrif-docno">
+<div><span class="mrif-dn-label">MRIF No.:</span><span class="mrif-dn-box">${docNo}</span></div>
+<div class="mrif-doc-qr"><img src="${mrifQrUrl}" alt="MRIF QR" style="width:90px;height:90px;margin-top:4px;"></div>
+</div>
 </div>
 <div class="mrif-title">MATERIALS REQUEST AND ISSUANCE FORM</div>
 <table class="mrif-meta">
 <tr>
 <td class="meta-label">REQUESTOR:</td>
-<td class="meta-value">${requestor}</td>
-<td class="meta-label">GEM SO No.:</td>
+<td class="meta-value" colspan="2">${requestor}</td>
+<td class="meta-label-right">GEM SO No.:</td>
 <td class="meta-blue">${gemSo}</td>
-<td class="meta-label">JO No.:</td>
+<td class="meta-label-right">JO No.:</td>
 <td class="meta-blue">${joNo}</td>
 </tr>
 <tr>
 <td class="meta-label">DEPARTMENT/SECTION:</td>
-<td class="meta-value">${department}</td>
-<td class="meta-label">CLIENT NAME:</td>
-<td class="meta-value" colspan="3">${client}</td>
+<td class="meta-value" colspan="4">${department}</td>
+<td class="meta-label-right">CLIENT NAME:</td>
+<td class="meta-value">${client}</td>
 </tr>
 <tr>
 <td class="meta-label">DATE:</td>
-<td class="meta-blue">${dateStr}</td>
-<td class="meta-label">PROJECT:</td>
+<td class="meta-blue" colspan="2">${dateStr}</td>
+<td class="meta-label-right">PROJECT:</td>
 <td class="meta-value" colspan="3">${project}</td>
 </tr>
 </table>
@@ -1666,7 +1670,7 @@ var html = `<div class="mrif-print-sheet">
 <th style="width:14%">ITEM<br>CODE</th>
 <th style="width:7%">QR<br>IMG</th>
 <th style="width:34%">ITEM DESCRIPTION</th>
-<th style="width:9%">QUANTITY</th>
+<th style="width:9%">REQ.<br>QTY</th>
 <th style="width:9%">ISSUED<br>QTY</th>
 <th style="width:7%">UNIT</th>
 <th style="width:15%">REMARKS</th>
